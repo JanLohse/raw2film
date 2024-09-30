aeb194997e5b20828ee40b8eeb7fb83832c3aeb5
JanLohse <jan.lars.lohse@gmail.com>
2024-09-30 15:03:30 +0800

    copy kelvin conversion

--- processing.py
+++ processing.py
@@ -176,16 +176,42 @@ class Raw2Film:
     @staticmethod
     def BT2020_to_kelvin(rgb):
         XYZ = colour.RGB_to_XYZ(rgb, "ITU-R BT.2020")
-        xy = colour.XYZ_to_xy(XYZ)
-        CCT = colour.xy_to_CCT(xy, "Hernandez 1999")
+        x, y = colour.XYZ_to_xy(XYZ)
+        n = (x - 0.3366) / (y - 0.1735)
+        CCT = (-949.86315 + 6253.80338 * np.exp(-n / 0.92159) + 28.70599 * np.exp(-n / 0.20039)
+               + 0.00004 * np.exp(-n / 0.07125))
         return CCT
 
-    def kelvin_to_BT2020(self, kelvin):
+    def kelvin_to_BT2020(self, CCT):
         global cp
         if not self.cuda:
             cp = np
-        xy = colour.CCT_to_xy(kelvin, "Kang 2002")
-        XYZ = colour.xy_to_XYZ(xy)
+
+        CCT_3 = CCT ** 3
+        CCT_2 = CCT ** 2
+
+        x = np.where(
+            CCT <= 4000,
+            -0.2661239 * 10 ** 9 / CCT_3
+            - 0.2343589 * 10 ** 6 / CCT_2
+            + 0.8776956 * 10 ** 3 / CCT
+            + 0.179910,
+            -3.0258469 * 10 ** 9 / CCT_3
+            + 2.1070379 * 10 ** 6 / CCT_2
+            + 0.2226347 * 10 ** 3 / CCT
+            + 0.24039,
+        )
+
+        x_3 = x ** 3
+        x_2 = x ** 2
+
+        cnd_l = [CCT <= 2222, np.logical_and(CCT > 2222, CCT <= 4000), CCT > 4000]
+        i = -1.1063814 * x_3 - 1.34811020 * x_2 + 2.18555832 * x - 0.20219683
+        j = -0.9549476 * x_3 - 1.37418593 * x_2 + 2.09137015 * x - 0.16748867
+        k = 3.0817580 * x_3 - 5.8733867 * x_2 + 3.75112997 * x - 0.37001483
+        y = np.select(cnd_l, [i, j, k])
+
+        XYZ = colour.xy_to_XYZ(cp.array([x, y]))
         rgb = colour.XYZ_to_RGB(XYZ, "ITU-R BT.2020")
         return cp.asarray(rgb)
 
@@ -463,7 +489,7 @@ class Raw2Film:
 
     @staticmethod
     def encode_ARRILogC3(x):
-        cut, a, b, c, d, e, f, _e_cut_f = 0.010591, 5.555556, 0.052272, 0.247190, 0.385537, 5.367655, 0.092809, 0.149658
+        cut, a, b, c, d, e, f = 0.010591, 5.555556, 0.052272, 0.247190, 0.385537, 5.367655, 0.092809
 
         return np.where(x > cut, c * np.log10(a * x + b) + d, e * x + f)
 
