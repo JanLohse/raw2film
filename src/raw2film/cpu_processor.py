fix cpu preview scaling

src/raw2film/cpu_processor.py
@@ -10,7 +10,7 @@ from spectral_film_lut.xy_lut import apply_2d_lut
 from wgpu import get_default_device
 
 from raw2film import effects
-from raw2film.effects import add_canvas, add_canvas_uniform, chroma_nr_filter
+from raw2film.effects import add_canvas, chroma_nr_filter
 from raw2film.raw_conversion import CANVAS_MODES, crop_rotate_zoom, raw_to_linear
 from raw2film.utils import (
     apply_lut_tetrahedral,
@@ -396,21 +396,7 @@ class CpuProcessor:
 
         image = (image * (2**8 - 1)).astype(np.uint8)
 
-        # post-processing (canvas, scaling)
-        if canvas_mode != "No":
-            if "white" in canvas_mode:
-                canvas_color = (255, 255, 255)
-            elif "black" in canvas_mode:
-                canvas_color = (0, 0, 0)
-            else:
-                canvas_color = (128, 128, 128)
-            if "Proportional" in canvas_mode:
-                canvas_ratio = image.shape[1] / image.shape[0]
-                image = add_canvas(image, canvas_ratio, canvas_scale, canvas_color)
-            elif "Fixed" in canvas_mode:
-                image = add_canvas(image, canvas_ratio, canvas_scale, canvas_color)
-            elif "Uniform" in canvas_mode:
-                image = add_canvas_uniform(image, canvas_scale, canvas_color)
+        image = add_canvas(image, canvas_mode, canvas_scale, canvas_ratio)
 
         if resolution is not None:
             image = resolution_scaling(image, resolution)

src/raw2film/effects.py
@@ -16,6 +16,8 @@ from spectral_film_lut.config import DEFAULT_DTYPE
 from spectral_film_lut.film_spectral import FilmSpectral
 from spectral_film_lut.grain_generation import generate_grain
 
+from raw2film.raw_conversion import CANVAS_MODES
+
 
 def lens_correction(
     rgb: np.ndarray, metadata: dict, cam: None | str, lens: None | str
@@ -286,44 +288,82 @@ def halation(
     return rgb
 
 
+def get_canvas_data(
+    image: np.ndarray,
+    canvas_mode: CANVAS_MODES,
+    canvas_scale: float = 1.0,
+    canvas_ratio: float = 1.0,
+):
+    if "white" in canvas_mode:
+        canvas_color = (255, 255, 255)
+    elif "black" in canvas_mode:
+        canvas_color = (0, 0, 0)
+    else:
+        canvas_color = (128, 128, 128)
+
+    if "Proportional" in canvas_mode:
+        canvas_ratio = image.shape[1] / image.shape[0]
+        img_ratio = image.shape[1] / image.shape[0]
+        if img_ratio > canvas_ratio:
+            output_resolution = (
+                int(image.shape[1] / canvas_ratio * canvas_scale),
+                int(image.shape[1] * canvas_scale),
+            )
+        else:
+            output_resolution = (
+                int(image.shape[0] * canvas_scale),
+                int(image.shape[0] * canvas_ratio * canvas_scale),
+            )
+        offset = np.subtract(output_resolution, image.shape[:2]) // 2
+    elif "Fixed" in canvas_mode:
+        img_ratio = image.shape[1] / image.shape[0]
+        if img_ratio > canvas_ratio:
+            output_resolution = (
+                int(image.shape[1] / canvas_ratio * canvas_scale),
+                int(image.shape[1] * canvas_scale),
+            )
+        else:
+            output_resolution = (
+                int(image.shape[0] * canvas_scale),
+                int(image.shape[0] * canvas_ratio * canvas_scale),
+            )
+        offset = np.subtract(output_resolution, image.shape[:2]) // 2
+    elif "Uniform" in canvas_mode:
+        side_length = max(image.shape[:2])
+        border_size = int(side_length * (canvas_scale - 1))
+        output_resolution = (image.shape[0] + border_size, image.shape[1] + border_size)
+        offset = np.subtract(output_resolution, image.shape[:2]) // 2
+
+    return output_resolution, canvas_color, offset
+
+
 def add_canvas(
     image: np.ndarray,
-    canvas_ratio: float,
-    canvas_scale: float,
-    canvas_color: tuple[int, int, int],
+    canvas_mode: CANVAS_MODES,
+    canvas_scale: float = 1.0,
+    canvas_ratio: float = 1.0,
 ):
     """Adds a background canvas to an image."""
-    img_ratio = image.shape[1] / image.shape[0]
-    if img_ratio > canvas_ratio:
-        output_resolution = (
-            int(image.shape[1] / canvas_ratio * canvas_scale),
-            int(image.shape[1] * canvas_scale),
-        )
-    else:
-        output_resolution = (
-            int(image.shape[0] * canvas_scale),
-            int(image.shape[0] * canvas_ratio * canvas_scale),
-        )
-    offset = np.subtract(output_resolution, image.shape[:2]) // 2
+    if canvas_mode == "No":
+        return image
+
+    output_resolution, canvas_color, offset = get_canvas_data(
+        image, canvas_mode, canvas_scale, canvas_ratio
+    )
+
+    output_resolution, canvas_color, offset = get_canvas_data(
+        image, canvas_mode, canvas_scale, canvas_ratio
+    )
+
+    print("image.shape =", image.shape)
+    print("output_resolution =", output_resolution)
+    print("offset =", offset)
+
     canvas = np.tensordot(np.ones(output_resolution), canvas_color, axes=0)
     canvas[
         offset[0] : offset[0] + image.shape[0], offset[1] : offset[1] + image.shape[1]
     ] = image
-    return canvas.astype(dtype="uint8")
 
-
-def add_canvas_uniform(
-    image: np.ndarray, canvas_scale: float, canvas_color: tuple[int, int, int]
-):
-    """Adds background canvas to image."""
-    side_length = max(image.shape[:2])
-    border_size = int(side_length * (canvas_scale - 1))
-    output_resolution = (image.shape[0] + border_size, image.shape[1] + border_size)
-    offset = np.subtract(output_resolution, image.shape[:2]) // 2
-    canvas = np.tensordot(np.ones(output_resolution), canvas_color, axes=0)
-    canvas[
-        offset[0] : offset[0] + image.shape[0], offset[1] : offset[1] + image.shape[1]
-    ] = image
     return canvas.astype(dtype="uint8")
 
 

src/raw2film/gui.py
@@ -2119,21 +2119,32 @@ class MainWindow(QMainWindow):
         image: np.ndarray,
         full_height: int,
         full_width: int,
-        img_height: int,
-        img_width: int,
     ):
-        canvas_rgba = np.zeros((full_height, full_width, 4), dtype=np.uint8)
-
         image = np.ascontiguousarray(image)
 
-        y_offset = (full_height - img_height) // 2
-        x_offset = (full_width - img_width) // 2
+        image_height, image_width = image.shape[:2]
+
+        canvas_ratio = full_width / full_height
+        image_ratio = image_width / image_height
+
+        if canvas_ratio > image_ratio:
+            canvas_height = image_height
+            canvas_width = round(image_height * canvas_ratio)
+        else:
+            canvas_width = image_width
+            canvas_height = round(image_width / canvas_ratio)
+
+        y_offset = (canvas_height - image_height) // 2
+        x_offset = (canvas_width - image_width) // 2
+
+        canvas_rgba = np.zeros((canvas_height, canvas_width, 4), dtype=np.uint8)
 
         canvas_rgba[
-            y_offset : y_offset + img_height, x_offset : x_offset + img_width, 0:3
+            y_offset : y_offset + image_height, x_offset : x_offset + image_width, 0:3
         ] = image
+
         canvas_rgba[
-            y_offset : y_offset + img_height, x_offset : x_offset + img_width, 3
+            y_offset : y_offset + image_height, x_offset : x_offset + image_width, 3
         ] = 255
 
         self.image_context.set_bitmap(canvas_rgba)
@@ -2202,7 +2213,7 @@ class MainWindow(QMainWindow):
             histogram = generate_histogram(image, height=80)
             self.histogram_context.set_bitmap(histogram)
             img_height, img_width, _ = image.shape
-            self.numpy_to_canvas(image, full_height, full_width, img_height, img_width)
+            self.numpy_to_canvas(image, full_height, full_width)
 
         self.histogram.request_draw()
         self.image.request_draw()